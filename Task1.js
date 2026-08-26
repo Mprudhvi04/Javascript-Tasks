3c81e69d585becda855be113eb93c92256646111
Mprudhvi04 <131843319+Mprudhvi04@users.noreply.github.com>
2026-08-26 23:14:51 +0530

    Add files via upload

---
+++ Task1.js
@@ -0,0 +1,23 @@
+var studentName="prudhvi";
+var age=23;
+var mark=90;
+console.log(studentName)
+console.log(age)
+console.log(mark)
+   
+// 
+
+let userName=prompt("Type your username here")
+alert(userName);
+// 
+
+let userAge=prompt("your Age");
+console.log(userAge);
+
+// 
+let question="Do you want to proceed ?";
+confirm(question);
+
+// 
+var  userNamee=prompt("Type your Username ");
+document.write(userNamee);
